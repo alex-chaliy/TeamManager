ee63bbb8bd695164378db0c38a21ead061f15e2f
Alex <alex.chaliy.42@gmail.com>
2017-08-31 21:05:42 +0300

    Front: fixed logs in app.config

--- client/src/app/shared/app.config.ts
+++ client/src/app/shared/app.config.ts
@@ -1,14 +1,15 @@
 import * as process from 'process';
 
 export let ApiServer = 'http://' + (process.env.NODE_ENV == 'DEV' ? 'localhost' : '77.220.213.35') + ':3030/';
-console.log('\n Api Server is: ', ApiServer);
+console.warn('\n Api Server is: ', ApiServer);
 
 function logDev() {
-	console.warn('\n if you want be in DEVELOPMENT mode & run your app on localhost: ' +  '\n\n set NODE_ENV=DEV \n');
+    console.warn('DEVELOPMENT mode is now.');
+	console.warn( 'NODE_ENV = ', process.env.NODE_ENV );
 }
 function logProd() {
     console.warn('PRODUCTION mode is now.');
-	console.warn( 'NODE_ENV = ', process.env.NODE_ENV );
+	console.warn('\n if you want to be in DEVELOPMENT mode & run your app on localhost: ', '\n set NODE_ENV=DEV \n');
 }
 process.env.NODE_ENV == 'DEV' && logDev;
 process.env.NODE_ENV != 'DEV' && logProd;
